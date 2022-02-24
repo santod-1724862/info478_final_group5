1ff6b2230cbe24d34d810834341804ac8960a217
parksd2 <parksd2@uw.edu>
2022-02-23 16:50:21 -0800

    Adding my analysis

--- EPA/analysis.R
+++ EPA/analysis.R
@@ -4,6 +4,14 @@ library(stringr)
 library(ggplot2)
 library(scales)
 
+
+accidents_by_temp <- read_csv('../Data/accident_data_large.csv')
+
+gbd_data <- read_csv('../Data/GBD_data.csv')
+
+gbd_age <- read_csv('../Data/gbd_age.csv')
+
+
 # DATA CLEANING
 
 # Motor_Vehicle_Occupant_Death_Rate__by_Age_and_Gender__2012___2014__All_States
@@ -62,3 +70,60 @@ death_ratio_map_data <- death_ratio_map %>%
 death_ratio_heatmap <- ggplot(death_ratio_map_data, aes(long, lat, group = group)) +
   geom_polygon(aes(fill = ratio), color = "white") +
   scale_fill_viridis_c(option = "C") + theme_void() + labs(title="Heatmap of Impaired Driving Deaths to Overall Driving Deaths")
+
+
+# statistical testing
+
+gbd <- gbd_data %>%
+  select(!c(measure_id, metric_id, upper, lower)) %>%
+  pivot_wider(names_from = measure_name, values_from = val)
+
+names(gbd)[names(gbd) == 'DALYs (Disability-Adjusted Life Years)'] <- 'DALYs'
+
+gbd <- gbd %>%
+  pivot_wider(names_from = metric_name, values_from = c(Deaths, DALYs))
+
+gbd <- gbd %>%
+  summarize(Total_Deaths = sum(Deaths_Number),
+            Average_Death_Rate = mean(Deaths_Rate),
+            Total_DALYs = sum(DALYs_Number),
+            Average_DALYs_Rate = mean(DALYs_Rate))
+
+
+# temperature analysis
+temps_ordered <- c('Freezing (Below 32)', 'Cold (between 32 and 49)', 'Mild (between 50 and 69)',
+                   'Warm (between 70 and 89)', 'Hot (above 90)')
+
+temps_grouped <- accidents_by_temp %>%
+  group_by(`temp_range(F)`) %>%
+  summarize(Accidents = n())
+
+temps_grouped$`temp_range(F)` <- factor(temps_grouped$`temp_range(F)`, levels = temps_ordered)
+
+
+temp_histogram <- accidents_by_temp %>%
+  ggplot(aes(x = `Temperature(F)`)) +
+  geom_histogram(binwidth = 3) +
+  labs(title = "Distribution of Accidents by Temperature", x = "Temperature (°F)")
+
+temp_col_chart <- temps_grouped %>%
+  ggplot(aes(x = `temp_range(F)`, y = Accidents)) +
+  geom_col() + labs(title = "Accidents per Temperature Range", x = 'Temperature Range (°F)', y = 'Total Accidents') +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+
+# age analysis
+age_accidents <- gbd_age %>%
+  filter(sex_name == "Both") %>%
+  filter(age_name != "All Ages") %>%
+  filter(metric_name == "Rate")
+
+age_death_dalys <- age_accidents %>%
+  select(!c(measure_id, upper, lower)) %>%
+  pivot_wider(names_from = measure_name, values_from = val)
+
+names(age_death_dalys)[names(age_death_dalys) == 'DALYs (Disability-Adjusted Life Years)'] <- 'DALYs'
+
+age_scatter <- age_death_dalys %>%
+  ggplot(aes(x = Deaths, y = DALYs, colour = age_name)) + geom_point() +
+  labs(title = "Death and DALYs Rate (per 100,000) Caused by Road Traffic Accidents, by State and Age Group",
+       x = "Death Rate", y = "DALYs Rate")
